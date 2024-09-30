d5712b6da9fc16c3312bd02ba7f689110be1ee6b
waleedauthornate <waleed@authornate.com>
2024-09-30 14:35:35 +0500

    All auths working - signer mutated

--- api/auth.ts
+++ api/auth.ts
@@ -1,66 +1,3 @@
-// import { PrismaClient } from "@prisma/client";
-
-// const prisma = new PrismaClient({
-//   datasources: {
-//     db: {
-//       // url: process.env.DB_URL,
-//       url: "postgresql://brain:yYrUPbXLRRK4jBXe8-J4xQ@brainedge-dev-15631.7tt.aws-us-east-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full",
-//     },
-//   },
-// });
-// // change
-// export default async function handler(req, res) {
-//   if (req.method === "POST") {
-//     const { username, email, privyId, walletSigner, avatarId } = req.body;
-//     console.log("username: " + username);
-//     console.log("email: " + email);
-//     console.log("walletSigner: " + walletSigner);
-//     console.log("privyId: " + username);
-//     console.log("avatarId: " + avatarId);
-//     console.log("----");
-
-//     if (!username && !email) {
-//       return res.status(400).json({ error: "Username or email is required" });
-//     }
-
-//     try {
-//       // Check if the user already exists
-//       const existingUser = await prisma.prismUser.findFirst({
-//         where: {
-//           OR: [
-//             { username: username ?? undefined },
-//             { email: email ?? undefined },
-//           ],
-//         },
-//       });
-
-//       if (existingUser) {
-//         return res
-//           .status(200)
-//           .json({ message: "Welcome back!", user: existingUser });
-//       }
-
-//       // Create a new user if one doesn't exist
-//       const newUser = await prisma.prismUser.create({
-//         data: {
-//           username,
-//           email,
-//           privyId,
-//           walletSigner,
-//           avatarId,
-//         },
-//       });
-
-//       return res.status(201).json({ message: "User created", user: newUser });
-//     } catch (error) {
-//       return res
-//         .status(500)
-//         .json({ error: "Something went wrong", details: error.message });
-//     }
-//   } else {
-//     return res.status(405).json({ error: "Method not allowed" });
-//   }
-// }
 import { Prisma, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({
@@ -75,7 +12,7 @@ const prisma = new PrismaClient({
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const { username, email, privyId, avatarId } = req.body;
-    const walletSigner = Math.random().toString();
+    const walletSigner = Math.random().toString().replace(".", "x");
     console.log("username: " + username);
     console.log("email: " + email);
     console.log("walletSigner: " + walletSigner);
