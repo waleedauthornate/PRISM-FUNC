2a36786e50b116472c9ff27dd908d56da67e0332
waleedauthornate <waleed@authornate.com>
2024-09-30 11:29:20 +0500

    Log messages

--- api/auth.ts
+++ api/auth.ts
@@ -75,7 +75,7 @@ const prisma = new PrismaClient({
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const { username, email, privyId, avatarId } = req.body;
-    const walletSigner = Math.random();
+    const walletSigner = Math.random().toString();
     console.log("username: " + username);
     console.log("email: " + email);
     console.log("walletSigner: " + walletSigner);
